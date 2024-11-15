1a285a630b7fc06ad5905aed3384c64b9f21fa46
jxxyx <jxxyx21@gmail.com>
2024-11-15 22:02:13 +0800

    Feat: Updated User Profile and Task 1 Model

---
+++ CloudInterface/myapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import UserProfile, Task1
+
+# Registering the models
+admin.site.register(UserProfile)
+admin.site.register(Task1)
---
+++ CloudInterface/myapp/models.py
@@ -0,0 +1,19 @@
+# import models from django.db
+from django.db import models
+
+# Defining the User class here to store the user details
+class UserProfile(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+    # returns it as a string in the /admin portion of the website
+    def __str__(self):
+        return self.username
+    
+class Task1(models.Model):
+    airline = models.CharField(max_length=50)
+    count = models.IntegerField(default=False)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.task1
