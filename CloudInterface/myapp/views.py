aab023c657140c23435fd6e2ead28cd9b5663363
jxxyx <jxxyx21@gmail.com>
2024-11-16 16:36:20 +0800

    Feat: Added in buttons to view the task visualization

--- CloudInterface/myapp/views.py
+++ CloudInterface/myapp/views.py
@@ -9,5 +9,17 @@ def landing_page(request):
     get_template('landing_page.html')
     return render(request, 'landing_page.html')
 
-def test_page(request):
-    return HttpResponse("<h1>Test Page</h1>")
+def task1(request):
+    return render(request, 'task1.html')
+
+def task2(request):
+    return render(request, 'task2.html')
+
+def task3(request):
+    return render(request, 'task3.html')
+
+def task4(request):
+    return render(request, 'task4.html')
+
+def task5(request):
+    return render(request, 'task5.html')
