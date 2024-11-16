bb91a338eab53b37ac31d1e6172b20eabf035a67
jxxyx <jxxyx21@gmail.com>
2024-11-16 18:08:01 +0800

    Feat: Task1 Visualization up

--- CloudInterface/myapp/urls.py
+++ CloudInterface/myapp/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
-    path('task1/', views.task1, name='task1'),
+    path('task1/', views.task1_view, name='task1'),
     path('task2/', views.task2, name='task2'),
-    path('task2/', views.task3, name='task3'),
-    path('task2/', views.task4, name='task4'),
-    path('task2/', views.task5, name='task5')
+    path('task3/', views.task3, name='task3'),
+    path('task4/', views.task4, name='task4'),
+    path('task5/', views.task5, name='task5'),
 ]
--- CloudInterface/myapp/views.py
+++ CloudInterface/myapp/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.loader import get_template
+from .models import Task1
+import json
 
 # Create your views here.
 
@@ -9,8 +11,20 @@ def landing_page(request):
     get_template('landing_page.html')
     return render(request, 'landing_page.html')
 
-def task1(request):
-    return render(request, 'task1.html')
+from django.shortcuts import render
+from .models import Task1
+
+def task1_view(request):
+    data = Task1.objects.all()
+    labels = [item.airline for item in data]
+    values = [item.count for item in data]
+
+    # Pass JSON-encoded data to the template
+    context = {
+        'labels': json.dumps(labels),  # JSON-encode the labels
+        'data': json.dumps(values),   # JSON-encode the values
+    }
+    return render(request, 'task1.html', context)
 
 def task2(request):
     return render(request, 'task2.html')
