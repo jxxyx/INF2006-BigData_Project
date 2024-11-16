2a70ed9c0591097265fcd2e1c4db1b9589130021
jxxyx <jxxyx21@gmail.com>
2024-11-16 13:10:36 +0800

    Feat: Landing page css and html. Urls.py and Views.py

--- CloudInterface/myapp/views.py
+++ CloudInterface/myapp/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import get_template
 
 # Create your views here.
+
+# This is the landing page view
+def landing_page(request):
+    get_template('landing_page.html')
+    return render(request, 'landing_page.html')
+
+def test_page(request):
+    return HttpResponse("<h1>Test Page</h1>")
